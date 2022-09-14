4738ced04d4cd2bb756b12bdc0ae26b5ded4480c
Iuri-Naturesa <98425830+Iuri-Naturesa@users.noreply.github.com>
2022-09-14 03:09:32 -0300

    responsividade h1

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const mobileButton = document.querySelector(".menu-btn");
+const mobileLink = document.querySelectorAll(".listaJS");
 
 function toggleMenu() {
   const mobileMenu = document.querySelector(".header-links");
   mobileMenu.classList.toggle("ativo");
 }
 mobileButton.addEventListener("click", toggleMenu);
+
+mobileLink.forEach((event) => {
+  event.addEventListener("click", toggleMenu);
+});
